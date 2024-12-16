d1a5a5243ce6b623b6ff8ed462507ec5413576ba
Idrisse ANGAMA <idrisseangama2a@gmail.com>
2024-12-16 15:24:15 +0100

    Quest 3 done

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String movieName = "Indiana Jones et la Dernière Croisade";
+        boolean hasWatchedTheMovie = false;
+        int movieReleaseDate = 1989;
+        double movieRating = 8.2;
+
+        System.out.println("Nom du film : "+ movieName);
+        System.out.println("Est ce que je l'ai regardé ? "+ hasWatchedTheMovie);
+        System.out.println("Le film est sorti en : " + movieReleaseDate);
+        System.out.println("Sur IMDB, il est noté : " + movieRating + "/10");
+
+
+    }
+}
